eb2ce6a8783623e12a7879a540b4d69358a75541
Klvst3r <klvst3r@gmail.com>
2023-02-10 13:43:33 -0600

    6.3.2. Estilos en mensajes de error de validación de forms

--- resources/views/contactos.blade.php
+++ resources/views/contactos.blade.php
@@ -7,15 +7,15 @@
 		<form method="POST" action="contacto">
 			<p><label for="nombre">Nombre
 				<input type="text" name="nombre" value="{{ old('nombre') }}">
-				{{ $errors->first('nombre') }}
+				{!! $errors->first('nombre','<span class=error>:message</span>') !!}
 			</label></p>
 			<p><label for="email">Email
 				<input type="email" name="email" value="{{ old('email') }}">
-					{{ $errors->first('email') }}
+					{!! $errors->first('email','<span class=error>:message</span>') !!}
 			</label></p>
 			<p><label for="mensaje">Mensaje
 				<textarea name="mensaje">{{ old('mensaje') }}</textarea>
-				{{ $errors->first('mensaje') }}
+				{!! $errors->first('mensaje','<span class=error>:message</span>') !!}
 			</label></p>
 			<p><input type="submit" value="Enviar">
 		</form>
